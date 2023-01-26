392f7a34a595b724397e23fe8db67d29021923b1
Brahma Putra <brahmaputra1996@gmail.com>
2023-01-26 22:20:15 +0700

    update allowed origins

--- server.js
+++ server.js
@@ -3,9 +3,22 @@ const cors = require('cors')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
+
+
+const allowedOrigins = ['http://localhost:3185', 'https ://prisign-brahma.netlify.app']
+
 app.use(cors({
-  origin:'*'
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  }
 }))
+
 app.use(createProxyMiddleware({
   router: (req) => new URL(req.path.substring(1)),
   pathRewrite: (path, req) => (new URL(req.path.substring(1))).pathname,
